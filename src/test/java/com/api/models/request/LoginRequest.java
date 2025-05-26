3b60622677b9165c38a75c0c1895fb9ad28c062b
Amitprasadsb2 <amitprasad.sb2@gmail.com>
2025-05-26 12:59:08 +0530

    Added-xml-file-for-regression

--- src/test/java/com/api/models/request/LoginRequest.java
+++ src/test/java/com/api/models/request/LoginRequest.java
@@ -4,9 +4,9 @@ public class LoginRequest {
     private String username;
     private String password;
 
-    public LoginRequest(String password, String username) {
-        this.password = password;
+    public LoginRequest(String username, String password) {
         this.username = username;
+        this.password = password;
     }
 
     public String getUsername() {
