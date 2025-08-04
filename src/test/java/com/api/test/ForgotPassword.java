07e4d6a936b5e22a288c116b66349724acca25b3
Amitprasadsb2 <amitprasad.sb2@gmail.com>
2025-08-04 13:19:10 +0530

    Comment CLean up done

--- src/test/java/com/api/test/ForgotPassword.java
+++ src/test/java/com/api/test/ForgotPassword.java
@@ -8,7 +8,6 @@ public class ForgotPassword {
     @Test(description = "Verify the Forgot password API")
 
     public void forgotpasswordtest(){
-    //update niharika changes
         AuthService authService = new AuthService();
         Response response = authService.forgotPassword("sanat@gmail.com");
         System.out.println(response.asPrettyString());
